3cbdb9c5e377b4e0bf2308fc227909cf3d4e57e7
Mark Prins <markprins@b3partners.nl>
2015-12-17 18:39:12 +0100

    update factsheet view vanuit db
    workflow_status is verwijderd vanwege flamingo-geocms/flamingo#497 maar is ook niet nodig voor de factsheet.

--- src/main/ddl/v_factsheet_terrein_info.sql
+++ src/main/ddl/v_factsheet_terrein_info.sql
@@ -4,21 +4,65 @@
 
 CREATE OR REPLACE VIEW "IBIS".v_factsheet_terrein_info AS 
  SELECT bedrijventerrein.id AS terreinid,
-    bedrijventerrein.*,
+    bedrijventerrein.id,
+    bedrijventerrein.rin_nr,
+    bedrijventerrein.datummutatie,
+    bedrijventerrein.reden,
+    bedrijventerrein.a_bestemming,
+    bedrijventerrein.a_grootstedeel,
+    bedrijventerrein.a_haruimtegebruik,
+    bedrijventerrein.a_kernnaam,
+    bedrijventerrein.a_ovwkavelgrootte,
+    bedrijventerrein.a_planfase,
+    bedrijventerrein.a_plannaam,
+    bedrijventerrein.a_faseveroudering,
+    bedrijventerrein.a_statusrpb,
+    bedrijventerrein.a_type,
+    bedrijventerrein.c_hyperlink,
+    bedrijventerrein.c_onderhoudemail,
+    bedrijventerrein.c_onderhoudnaam,
+    bedrijventerrein.c_onderhoudtelefoon,
+    bedrijventerrein.c_organisatie,
+    bedrijventerrein.c_postcodeplaats,
+    bedrijventerrein.c_verkoopadres,
+    bedrijventerrein.c_verkoopemail,
+    bedrijventerrein.c_verkoopnaam,
+    bedrijventerrein.c_verkooptelefoon,
+    bedrijventerrein.c_verkoopwebsite,
+    bedrijventerrein.o_afstandvliegveld,
+    bedrijventerrein.o_collbeheer,
+    bedrijventerrein.o_collinkoop,
+    bedrijventerrein.o_collvoorz,
+    bedrijventerrein.o_internet,
+    bedrijventerrein.o_maxhuur,
+    bedrijventerrein.o_maxverkoop,
+    bedrijventerrein.o_milieuwet,
+    bedrijventerrein.o_milieuzone,
+    bedrijventerrein.o_externebereikbaarheid,
+    bedrijventerrein.o_minhuur,
+    bedrijventerrein.o_minverkoop,
+    bedrijventerrein.o_naamvliegveld,
+    bedrijventerrein.o_overslag,
+    bedrijventerrein.o_parkeergelegenheid,
+    bedrijventerrein.o_spoorontsluiting,
+    bedrijventerrein.o_waterontsluiting,
+    bedrijventerrein.o_wegontsluiting,
+    bedrijventerrein.gemeenteid,
+    bedrijventerrein.geom,
         CASE
-            WHEN   OWNER.beschikbare_panden IS NULL THEN 0::bigint
-            ELSE   OWNER.beschikbare_panden
+            WHEN owner.beschikbare_panden IS NULL THEN 0::bigint
+            ELSE owner.beschikbare_panden
         END AS beschikbare_panden,
-    round(v_terrein_oppervlakte.opp_geom::numeric, 2) AS opp_geom_ha,
-    round(v_terrein_oppervlakte.opp_woonbebouwing::numeric, 2) AS opp_woonbebouwing_ha,
-    round(v_terrein_oppervlakte.opp_openbare_ruimte::numeric, 2) AS opp_openbare_ruimte_ha,
-    round(v_terrein_oppervlakte.opp_niet_terstond_uitgeefbaar_part::numeric + v_terrein_oppervlakte.opp_niet_terstond_uitgeefbaar_gem::numeric, 2) AS opp_niet_terstond_uitgeefbaar_ha,
-    round(v_terrein_oppervlakte.opp_uitgegeven::numeric, 2) AS opp_uitgegeven_ha,
-    round(v_terrein_oppervlakte.opp_uitgeefbaar_part::numeric+v_terrein_oppervlakte.opp_uitgeefbaar_gem::numeric, 2) AS opp_uitgeefbaar_ha,
-    round(v_terrein_oppervlakte.opp_niet_bekend::numeric, 2) AS opp_niet_bekend_ha,
-    round(v_terrein_oppervlakte.opp_leeg::numeric, 2) AS opp_leeg_ha,
-    round(v_terrein_oppervlakte.opp_netto::numeric, 2) AS opp_netto_ha,
-    round(v_terrein_oppervlakte.opp_bruto::numeric, 2) AS opp_bruto_ha,
+    round(v_terrein_oppervlakte.opp_geom, 2) AS opp_geom_ha,
+    round(v_terrein_oppervlakte.opp_woonbebouwing, 2) AS opp_woonbebouwing_ha,
+    round(v_terrein_oppervlakte.opp_openbare_ruimte, 2) AS opp_openbare_ruimte_ha,
+    round(v_terrein_oppervlakte.opp_niet_terstond_uitgeefbaar_part + v_terrein_oppervlakte.opp_niet_terstond_uitgeefbaar_gem, 2) AS opp_niet_terstond_uitgeefbaar_ha,
+    round(v_terrein_oppervlakte.opp_uitgegeven, 2) AS opp_uitgegeven_ha,
+    round(v_terrein_oppervlakte.opp_uitgeefbaar_part + v_terrein_oppervlakte.opp_uitgeefbaar_gem, 2) AS opp_uitgeefbaar_ha,
+    round(v_terrein_oppervlakte.opp_niet_bekend, 2) AS opp_niet_bekend_ha,
+    round(v_terrein_oppervlakte.opp_leeg, 2) AS opp_leeg_ha,
+    round(v_terrein_oppervlakte.opp_netto, 2) AS opp_netto_ha,
+    round(v_terrein_oppervlakte.opp_bruto, 2) AS opp_bruto_ha,
     v_gemeente_en_regio_envelopes.naam AS gemeente_naam,
     v_gemeente_en_regio_envelopes.cbscode,
     v_gemeente_en_regio_envelopes.vvr_naam,
@@ -27,11 +71,13 @@ CREATE OR REPLACE VIEW "IBIS".v_factsheet_terrein_info AS
     v_gemeente_en_regio_envelopes.provincie,
     v_totaal_bedrijven_en_medewerkers_op_rin_nr.bedrijven AS aantal_bedrijven,
     v_totaal_bedrijven_en_medewerkers_op_rin_nr.medewerkers AS aantal_werkzame_personen
-   FROM "IBIS".bedrijventerrein
-     LEFT JOIN   OWNER ON bedrijventerrein.id =   OWNER.terreinid
-     LEFT JOIN "IBIS".v_terrein_oppervlakte ON v_terrein_oppervlakte.id = bedrijventerrein.id
-     LEFT JOIN "IBIS".v_gemeente_en_regio_envelopes ON bedrijventerrein.gemeenteid = v_gemeente_en_regio_envelopes.gem_id
-     LEFT JOIN "IBIS".v_totaal_bedrijven_en_medewerkers_op_rin_nr ON bedrijventerrein.rin_nr = v_totaal_bedrijven_en_medewerkers_op_rin_nr.rin_nr;
+   FROM bedrijventerrein
+     LEFT JOIN owner ON bedrijventerrein.id = owner.terreinid
+     LEFT JOIN v_terrein_oppervlakte ON v_terrein_oppervlakte.id = bedrijventerrein.id
+     LEFT JOIN v_gemeente_en_regio_envelopes ON bedrijventerrein.gemeenteid = v_gemeente_en_regio_envelopes.gem_id
+     LEFT JOIN v_totaal_bedrijven_en_medewerkers_op_rin_nr ON bedrijventerrein.rin_nr = v_totaal_bedrijven_en_medewerkers_op_rin_nr.rin_nr;
 
 ALTER TABLE "IBIS".v_factsheet_terrein_info
-  OWNER TO geo;
+  OWNER TO ibis;
+COMMENT ON VIEW "IBIS".v_factsheet_terrein_info
+  IS 'Geeft de terrein informatie bij een kavel tbv de factsheet. workflow_status is verwijderd vanwege https://github.com/flamingo-geocms/flamingo/issues/497 maar is ook niet nodig voor de factsheet.';
